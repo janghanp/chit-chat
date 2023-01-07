9c5a70c95daae7935b47d31ea373d84cc270fdcc
Janghan Park <janghan0412@gmail.com>
2023-01-07 12:12:33 +0900

    Add Prisma and Delete Dockerfile for now

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(morgan("dev"));
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({ hello: "what about this one" });
+});
+
+app.get("/users", async (_req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+
+  console.log(users);
+
+  res.status(200).json({ message: "success", data: users });
 });
 
 const port = Number(process.env.PORT) || 8080;
