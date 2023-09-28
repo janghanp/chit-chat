5fd3c186f6a1f852228af7a1ed1205df8b1000b5
janghan0412@gmail.com <janghan0412@gmail.com>
2023-09-28 12:20:00 +1000

    Change socket url

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'https://server.chichat.lat' : 'ws://localhost:5000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://server.chitchat.lat' : 'ws://localhost:5000';
 
 export const socket = io(URL as string, { autoConnect: true });
