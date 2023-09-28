0a53591a5cbe8b3992f4ac00c5a1f24fe5158a02
janghan0412@gmail.com <janghan0412@gmail.com>
2023-09-28 12:04:03 +1000

    Change socket url

--- backend/src/routes/socketRoute.ts
+++ backend/src/routes/socketRoute.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
     return res.status(200).json({ socket: 'socket' });
 });
 
